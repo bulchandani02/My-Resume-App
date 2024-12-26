88c5c6d030355082acc7c30e29d6e4cac6123d0c
bulchandani02 <Jyotsna.bulchandani02@gmail.com>
2024-12-26 15:19:19 +0530

    Create Work Experience.py

---
+++ pages/Work Experience.py
@@ -0,0 +1,45 @@
+import streamlit as st
+
+## Title of the page
+st.title("Professional Experience")
+
+## Work Experience - 1
+st.subheader("GenAI Automation Expert [TCS] ,[Aug-2024 to Present]")
+
+st.write("For about last 6 months, I am as an internal GenAI evangelist in Mainframe Modernization \
+        team where I am helping customers to understand Large Language Model Prompting, \
+        keeping a track on latest tools and trends in. In day-to-day work, I have discussions \
+        with stack holders for a deep understanding of various customer requirements and \
+        proposing/planning GenAI platforms to automate them. Working on prompt library \
+        and internal PoCs for various GenAI platforms like WatsonX code assistant, GitHub Co-Pilot\
+        , OpenAI, Claude, Llama, Falcon etc.")
+
+## Work Experience - 2
+st.subheader("Technical Architect- Legacy Modernization [TCS] ,[July-2021 to Aug-2024]")
+
+st.write("For about 3 years, I worked as a technical architect for Mainframe Modernization team \
+         where my day-to-day tasks were to interact directly with business teams / clients to understand \
+         their business strategy, Program requirements & processes and applies specialist knowledge to design \
+         & create digital technology/application solutions. Also, writing RFP responses, doing PoCs, \
+         proposing solution were my key responsibilities. ")
+
+## Work Experience - 3
+st.subheader("Application Developer – zDevops, RESTful APIs [TCS] ,[Feb-2019 to July-2021]")
+
+st.write("For about 3 years, I worked as a application developer for one of the UK based bank and migrated \
+         their legacy system to zDevops. Also, during this time I was leading team of 3 developers where we \
+         used to create RESTful APIs for core banking systems.")
+
+## Work Experience - 4
+st.subheader("Senior Software Developer – Mainframe applications (Vantage) [DXC] ,[Aug-2018 to Feb-2019]")
+
+st.write("I worked as a Senior Software Developer for one of the US based Insurance organization. \
+         I used to work on their policy administration system where my day-to-day tasks were to implement\
+         , debug and test the code. Gather the requirements and creation of technical documents etc.")
+
+## Work Experience - 5
+st.subheader("Software Developer – Mainframe applications(Vantage & wma) [Cognizant] ,[Apr-2016 to Aug-2018]")
+
+st.write("I started working with CTS in mainframe technology domain, where I got the classroom training from \
+         CTS trainers for about 6 months and then I was deployed to one of the Insurance clients. There, \
+         I worked as a software developer. ")
