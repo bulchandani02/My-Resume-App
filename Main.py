fe11323668ed57d472981f19834aa71813703d4a
bulchandani02 <Jyotsna.bulchandani02@gmail.com>
2024-12-26 15:18:18 +0530

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+## Title of the page
+st.title("Jyotsna Bulchandani")
+
+## Adding an image to Streamlit page
+st.image("Jyotsna_Photo.jpg", width=250)
+
+## Displying on page
+st.write("I have a strong foundation in LLMs, HuggingFace, LangChain, and Streamlit. \
+         Have been working for over a year now on building sophisticated LLM-powered applications, \
+         including chatbots, question-answering systems, and document summarization tools \
+         and their deployment. I am passionate about exploring the advancements in GenAI and \
+         contributing to the field ")
+
+st.write("**Email** - *d.jyotsna1987@gmail.com* ")
+
+st.write("**Linkedin** - *https://www.linkedin.com/in/jyotsna-bulchandani-3b4381112/* " )
+
+st.write("**GitHub** - *https://github.com/bulchandani02/* ")
+
